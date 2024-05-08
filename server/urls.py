07213bd18f8bcbad08a8da29c51acfd64d88d878
yasuomaidana <yasuomaidana@gmail.com>
2024-05-08 12:28:33 +0900

    Creating general view endpoint

--- server/urls.py
+++ server/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from backend import views
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("general/", views.general_view),
 ]
